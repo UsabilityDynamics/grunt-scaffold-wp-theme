f0c37a24619c63f54f7183d4d329b5575c023fae
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2013-11-18 13:32:06 +0200

    Updated gruntfile.js and app.dev.js. Added .htaccess

--- root/gruntfile.js
+++ root/gruntfile.js
@@ -8,7 +8,159 @@
 module.exports = function( grunt ) {
 
   grunt.initConfig({
+  
+    // Load configuration about project
+    pkg: grunt.file.readJSON( 'composer.json' ),
+    
+    uglify: {
+      'production': {
+        options: {
+          mangle: false,
+          beautify: false
+        },
+        files: {
+          'scripts/app.js':     [ 'scripts/src/app.dev.js' ],
+          'scripts/require.js': [ 'vendor/usabilitydynamics/lib-utility/scripts/require.js' ]
+        }
+      }
+    },
+    
+    requirejs: {
+    
+    },
+    
+    // Documentation
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        logo: 'http://media.usabilitydynamics.com/logo.png',
+        options: {
+          paths: './',
+          outdir: 'static/codex'
+        }
+      }
+    },
+    
+    // Less Compilation
+    less: {
+      'app.css': {
+        options: {
+          yuicompress: true,
+          relativeUrls: true
+        },
+        files: {
+          'styles/app.css': [ 'styles/less/app.less' ]
+        }
+      },
+      'app.dev.css': {
+        options: {
+          relativeUrls: true
+        },
+        files: {
+          'styles/app.dev.css': [ 'styles/less/app.less' ]
+        }
+      },
+      'editor-style.dev.css': {
+        options: {
+          yuicompress: false,
+          relativeUrls: true
+        },
+        files: {
+          'styles/editor-style.dev.css': [ 'styles/less/editor-style.less' ]
+        }
+      },
+      'editor-style.css': {
+        options: {
+          yuicompress: true,
+          relativeUrls: true
+        },
+        files: {
+          'styles/editor-style.css': [ 'styles/less/editor-style.less' ]
+        }
+      }
+    },
+
+    // Watcher
+    watch: {
+      options: {
+        interval: 1000,
+        debounceDelay: 500
+      },
+      styles: {
+        files: [
+          'gruntfile.js', 'styles/less/*.*'
+        ],
+        tasks: [ 'less' ]
+      },
+      scripts: {
+        files: [
+          'gruntfile.js', 'scripts/src/*.js'
+        ],
+        tasks: [ 'uglify', 'requirejs' ]
+      },
+      docs: {
+        files: [
+          'styles/app.*.css', 'composer.json', 'readme.md'
+        ],
+        tasks: [ 'markdown' ]
+      }
+    },
+    
+    markdown: {
+      all: {
+        files: [
+          {
+            expand: true,
+            src: 'readme.md',
+            dest: 'static/',
+            ext: '.html'
+          }
+        ],
+        options: {
+          markdownOptions: {
+            gfm: true,
+            codeLines: {
+              before: '<span>',
+              after: '</span>'
+            }
+          }
+        }
+      }
+    }
 
   });
+  
+  // Load tasks
+  grunt.loadNpmTasks( 'grunt-component' );
+  grunt.loadNpmTasks( 'grunt-component-build' );
+  grunt.loadNpmTasks( 'grunt-markdown' );
+  grunt.loadNpmTasks( 'grunt-requirejs' );
+  grunt.loadNpmTasks( 'grunt-spritefiles' );
+  grunt.loadNpmTasks( 'grunt-contrib-symlink' );
+  grunt.loadNpmTasks( 'grunt-contrib-yuidoc' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-less' );
+  //grunt.loadNpmTasks( 'grunt-contrib-concat' );
+  //grunt.loadNpmTasks( 'grunt-contrib-clean' );
+  //grunt.loadNpmTasks( 'grunt-shell' );
+  
+  // Build Assets
+  grunt.registerTask( 'default', [ 'yuidoc', 'uglify', 'markdown', 'less', 'requirejs' ] );
+
+  // Install environment
+  grunt.registerTask( 'install', [ 'yuidoc', 'uglify', 'markdown', 'less', 'requirejs' ] );
+
+  // Update Environment
+  grunt.registerTask( 'update', [ 'yuidoc', 'uglify', 'markdown', 'less', 'requirejs' ] );
+
+  // Prepare distribution
+  grunt.registerTask( 'dist', [ 'yuidoc', 'uglify', 'markdown', 'less', 'requirejs' ] );
+
+  // Update Documentation
+  grunt.registerTask( 'doc', [ 'yuidoc', 'markdown' ] );
 
 };
---
+++ root/scripts/src/app.dev.js
@@ -0,0 +1,46 @@
+/**
+ * Application Loader
+ *
+ */
+function Bootstrap( jQuery, async ) {
+  
+  // All events, functionality should be added here
+  
+}
+
+require.config({
+  "paths": {
+    "async": "//cdnjs.cloudflare.com/ajax/libs/async/0.2.7/async.min",
+    "html5shiv": "//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js",
+    "jquery.form": "//cdn.jsdelivr.net/jquery.form/3.36/jquery.form.min",
+    "bootstrap": "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min",
+    "knockout": "//ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1",
+    "lodash": "//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.2.1/lodash.min",
+    "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min",
+    "jquery": "//codeorigin.jquery.com/jquery-2.0.3"
+  },
+  "shim": {
+    "bootstrap": {
+      "deps": [ "jquery" ]
+    },
+    "backbone": {
+      "deps": [ "jquery", "lodash" ],
+      "exports": "Backbone"
+    },
+    "lodash": {
+      "exports": "_"
+    },
+    "knockout": {
+      "exports": "knockout"
+    }
+  },
+  "uglify": {
+    "beautify": true,
+    "max_line_length": 1000,
+    "no_mangle": true
+  },
+  "waitSeconds": 5
+});
+
+require( [ 'jquery', 'async', 'bootstrap' ], Bootstrap );
+
