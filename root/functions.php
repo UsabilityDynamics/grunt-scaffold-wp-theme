a851d3a734ce3f8e15935964ef867f1aaa2b5524
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2013-12-18 16:12:26 +0200

    minor typo fix

--- root/functions.php
+++ root/functions.php
@@ -10,7 +10,7 @@ namespace UsabilityDynamics\Theme {
 
   // Be sure that vendors installed ( composer install ). See: http://getcomposer.org/
   if( !file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-    wp_die( '<h1>Critical Error</h1>' . '<p>The website is currently being updated, please wait a few moments.</p><p>The NightCulture theme is missing the vendor directory, the theme appears to be unbuilt.</p>' );
+    wp_die( '<h1>Critical Error</h1>' . '<p>The website is currently being updated, please wait a few moments.</p><p>Theme is missing the vendor directory, the theme appears to be unbuilt.</p>' );
   }
 
   // Load vendor classes
