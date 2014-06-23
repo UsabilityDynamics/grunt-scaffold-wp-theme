2b1819940f7b73afcee92a472107c1224de1228e
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-23 08:05:21 -0400

    wip

--- tasks/install.js
+++ tasks/install.js
@@ -6,7 +6,10 @@ module.exports = function( grunt ) {
   
     var done = this.async();
   
-    symlink( __dirname, '/Users/potanin/.grunt-init/wp-theme', 'dir', function( error ) {      
+    symlink( require( 'path' ).dirname( __dirname ), '/Users/potanin/.grunt-init/wp-theme', 'dir', function( error ) {      
+
+      if( error && error.code === 'EEXIST' ) { return done(); }
+      
       console.log( 'linked', error ? error.message : 'successfully' );      
       done();      
     });      
